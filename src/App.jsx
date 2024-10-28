4f2154d49020c80ec0423cc85535d7011610d019
Farhan Ahmed <farhanf7n@gmail.com>
2024-10-28 23:47:24 +0500

    Made ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <Navbar />
+      <HeroSection />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "slate-1": "#111113",
+        "slate-2": "#18191b",
+        "slate-3": "#212225",
+      },
+    },
   },
   plugins: [],
 };
